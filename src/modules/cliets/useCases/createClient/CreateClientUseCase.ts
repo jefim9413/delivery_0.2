22bdb3d3b488262ed26859d12041cf622b72da32
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-12-22 16:22:40 -0300

    verificando se já existe username na base de dados

--- src/modules/cliets/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/cliets/useCases/createClient/CreateClientUseCase.ts
@@ -1,10 +1,19 @@
+import { prisma } from "../../../../database/prismaClient";
 interface ICreateClient {
   username: string;
   password: string;
 }
 
 class CreateClientUseCase {
-  async execute() {}
+  async execute({ password, username }: ICreateClient) {
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+  }
 }
 
 export { CreateClientUseCase };
