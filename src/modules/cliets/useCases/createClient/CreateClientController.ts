48f09a5661ddfebbb8681dffb97bc63dc5d53fb6
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-12-22 16:33:25 -0300

    iniciando função handle de controller

--- src/modules/cliets/useCases/createClient/CreateClientController.ts
+++ src/modules/cliets/useCases/createClient/CreateClientController.ts
@@ -1,3 +1,7 @@
-class CreateClientController {}
+import { Request, Response } from "express";
+
+class CreateClientController {
+  async handle(requet: Request, response: Response) {}
+}
 
 export { CreateClientController };
