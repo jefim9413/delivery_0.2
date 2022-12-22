9299efd4fa9757456889295f53bf36c177e5f423
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-12-22 16:17:04 -0300

    interface para atributos da entidade cliente

--- src/modules/cliets/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/cliets/useCases/createClient/CreateClientUseCase.ts
@@ -1,3 +1,10 @@
-class CreateClientUseCase {}
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+class CreateClientUseCase {
+  async execute() {}
+}
 
 export { CreateClientUseCase };
