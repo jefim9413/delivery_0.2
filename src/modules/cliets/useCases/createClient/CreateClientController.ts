2a65f55928a62c450e9f5ab763b73e03c9664267
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-12-22 16:36:34 -0300

    finalizando controller de createClient

--- src/modules/cliets/useCases/createClient/CreateClientController.ts
+++ src/modules/cliets/useCases/createClient/CreateClientController.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
+import { CreateClientUseCase } from "./CreateClientUseCase";
 
 class CreateClientController {
-  async handle(requet: Request, response: Response) {}
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+    const createClientUseCase = new CreateClientUseCase();
+
+    const result = await createClientUseCase.execute({ username, password });
+
+    return response.json(result);
+  }
 }
 
 export { CreateClientController };
